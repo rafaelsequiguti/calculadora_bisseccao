f1930758856b2ee0e7c52754c1e57064083aee19
Rafael Sequiguti <rafael.sequiguti@edu.unifil.br>
2020-09-18 19:43:18 -0300

    Metodo de Newton Implementado para uma funcao pre definida

--- app_v2.py
+++ app_v2.py
@@ -2,11 +2,12 @@ from tkinter import*
 import tkinter.font as tkFont
 import math
 import bissecao as bissecao
+import newton as newton
 
 class app():
     def __init__(self):
         self.janela = Tk()
-        self.janela.geometry("400x350")
+        self.janela.geometry("400x700")
         self.frame = Frame(self.janela)
         self.frame.pack(side=LEFT)
 
@@ -49,33 +50,85 @@ class app():
         self.emp1.grid(row=6, column = 0)
         
         # Entrada de dados 3
-        self.epslon = Label(self.frame, text = "epslon = ")
+        self.epslon = Label(self.frame, text = "Epslon = ")
         self.epslon.grid(row=7, column = 0)
 
         self.epslon_entry = Entry(self.frame, font=fontStyle1)
         self.epslon_entry.grid(row=7, column = 1)
         
         # Espaçamento
-        self.emp1 = Label(self.frame, text="")
-        self.emp1.grid(row=8, column = 0)
+        self.emp2 = Label(self.frame, text="")
+        self.emp2.grid(row=8, column = 0)
 
         # Button "igual"
-        self.bt = Button(self.frame, text = "=", command = self.conta)
-        self.bt.grid(row=9, column = 1)
+        self.bt2 = Button(self.frame, text = "=", command = self.conta_bissecaao)
+        self.bt2.grid(row=9, column = 1)
+        self.bt2.config(height = 2, width = 5)
+
+        # Resultado
+        self.biss = Label(self.frame, text="", font = fontStyle1)
+        self.biss.grid(row=10, column = 1)
+
+        # Espaçamento
+        self.emp2 = Label(self.frame, text="")
+        self.emp2.grid(row=11, column = 0)
+
+    ############################################################################################################
+        # Título
+
+        self.subtitle = Label(self.frame, text="Método de Newton", font = fontStyle2)
+        self.subtitle.grid(row=12, column = 1)
+
+        # Espaçamento
+        self.emp = Label(self.frame, text="")
+        self.emp.grid(row=13, column = 0)
+
+        
+        # Entrada de dados 1
+        self.chute_value = Label(self.frame, text = "Chute = ")
+        self.chute_value.grid(row=14, column = 0)
+
+        self.chute = Entry(self.frame, font=fontStyle1)
+        self.chute.grid(row=14, column = 1)
+
+        # Espaçamento
+        self.emp = Label(self.frame, text="")
+        self.emp.grid(row=15, column = 0)
+
+        # Entrada de dados 2
+        self.epslon = Label(self.frame, text = "Epslon = ")
+        self.epslon.grid(row=16, column = 0)
+
+        self.epslon_entry = Entry(self.frame, font=fontStyle1)
+        self.epslon_entry.grid(row=16, column = 1)
+
+        
+        # Espaçamento
+        self.emp1 = Label(self.frame, text="")
+        self.emp1.grid(row=17, column = 0)
+
+        # Button "igual"
+        self.bt = Button(self.frame, text = "=", command = self.conta_newton)
+        self.bt.grid(row=18, column = 1)
         self.bt.config(height = 2, width = 5)
 
         # Resultado
-        self.la = Label(self.frame, text="", font = fontStyle1)
-        self.la.grid(row=10, column = 1)
+        self.newt = Label(self.frame, text="", font = fontStyle1)
+        self.newt.grid(row=19, column = 1)
 
         self.janela.mainloop()        
 
-    def conta(self): # CALCULO
+    def conta_bissecaao(self): # CALCULO
         self.n = float(self.e.get())
         self.x = float(self.e1.get())
         self.eps = float(self.epslon_entry.get())
         self.result = float(bissecao.bisseccao(self.n,self.x,self.eps))
-        self.la["text"] = self.result
-    
-            
+        self.biss["text"] = self.result
+
+    def conta_newton(self):
+        self.entry1 = float(self.chute.get())
+        self.entry2 = float(self.epslon_entry.get())
+        self.result = float(newton.newton(self.entry1,self.entry2))
+        self.newt["text"] = self.result
+        
 app()
--- newton.py
+++ newton.py
@@ -3,4 +3,23 @@ import math
 def funcao (x):
 	return pow(x,2) + x - 6
 
-def newton(xo,):
+def derivada(x):
+	return 2*x + 1
+
+def newton(xo,limite):	
+
+	f = funcao(xo)
+	# x = xo - (funcao(xo)/derivada(xo))
+
+	f_aux = f
+	if(f_aux < 0): f_aux = f * - 1
+
+	while(f_aux > limite):
+		x = xo - (funcao(xo)/derivada(xo))
+
+		f = funcao(x)
+		f_aux = f
+		if(f_aux < 0): f_aux = f * - 1
+		xo = x
+	
+	return x
